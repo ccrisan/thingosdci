8462e8259200451dad781fbd355d6eb36f30d155
Calin Crisan <ccrisan@gmail.com>
2018-03-12 23:09:46 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup
+
+import thingosdci
+
+
+name = 'thingosdci'
+version = thingosdci.VERSION
+description = 'thingOS Docker CI'
+
+setup(
+    name=name,
+    version=version,
+
+    description=description,
+    long_description=description,
+
+    url='https://github.com/ccrisan/thingosdci',
+
+    author='Calin Crisan',
+    author_email='ccrisan@gmail.com',
+
+    license='GPLv3',
+
+    packages=[name],
+
+    install_requires=['tornado==4.5.2', 'redis==2.10.6'],
+
+    entry_points={
+        'console_scripts': [
+            'thingosdci=thingosdci.main:main',
+        ],
+    }
+)
