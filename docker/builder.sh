330d4c1d583b84aceffce6e89a62613632e9672f
Calin Crisan <ccrisan@gmail.com>
2025-10-26 22:43:37 +0200

    builder.sh: Revert mount changes

--- docker/builder.sh
+++ docker/builder.sh
@@ -25,15 +25,25 @@ export USER=root
 # tell git not to prompt for credentials
 export GIT_TERMINAL_PROMPT=0
 
+mkdir -p ${OS_DIR}
+cd ${OS_DIR}
 if [[ -n "${CHECKOUT}" ]]; then
     git clone ${TB_GIT_CLONE_ARGS} ${TB_REPO} .
 fi
-cd ${OS_DIR}
 if [[ -n "${TB_PR}" ]]; then
     git fetch origin pull/${TB_PR}/head:pr${TB_PR}
 fi
 if [[ -n "${CHECKOUT}" ]]; then
     git checkout ${CHECKOUT}
+
+    # prepare working dirs
+    mkdir -p /mnt/dl/${TB_BOARD}
+    mkdir -p /mnt/ccache/.buildroot-ccache-${TB_BOARD}
+    mkdir -p ${OS_DIR}/.buildroot-ccache-${TB_BOARD}
+    mkdir -p ${OS_DIR}/output
+
+    mount --bind /mnt/ccache/.buildroot-ccache-${TB_BOARD} ${OS_DIR}/.buildroot-ccache-${TB_BOARD}
+    mount --bind /mnt/output ${OS_DIR}/output
 fi
 
 if [[ -n "${TB_CUSTOM_CMD}" ]]; then
@@ -63,6 +73,12 @@ if [[ "${TB_CLEAN_TARGET_ONLY}" == "true" ]]; then
     ${OS_DIR}/build.sh ${TB_BOARD} clean-target
 else
     ${OS_DIR}/build.sh ${TB_BOARD} distclean
+fi
+
+# mount dl dir after cleanup, to preserve downloads
+if [[ -n "${CHECKOUT}" ]]; then
+    mkdir -p ${OS_DIR}/dl
+    mount --bind /mnt/dl/${TB_BOARD} ${OS_DIR}/dl
 fi
 
 # actual building
